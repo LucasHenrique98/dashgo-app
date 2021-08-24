f9ddec11366ea927d655fc1233bcf7545cb93b6f
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-08-23 23:17:45 -0300

    add responsiveness to the dashboard and users page

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,7 +4,7 @@ import { PaginationItem } from './PaginationItem';
 export default function Pagination() {
   return (
     <Stack
-      direction="row"
+      direction={['row', 'column']}
       mt="8"
       justify="space-between"
       align="center"
